8c7bc535c015b0ce74b4fb41d27a298afe85d8b4
SuperTNT17 <r.lemmers@outlook.com>
2026-03-26 15:11:05 +0100

    desktop style afgemaakt en onnodige fonts weggehaald

--- Two Blocks/src/Title.jsx
+++ Two Blocks/src/Title.jsx
@@ -1,14 +1,15 @@
 import './Title.css';
+import rightArrow from './assets/images/rightArrow2.svg';
 
 export function Title() {
     return (
         <div className='titleContainer'>
             <div className='labelTitleContainer'>
                 <p className='titleLabel'>Label</p>
-                <h1>Mandatory title about the call-to-action here</h1>
+                <h1 className='title'>Mandatory title about the<br></br><span>call-to-action</span> here</h1>
             </div>
             <p className='titleBody'>Optional body copy goes here.</p>
-            <a href="" className='titleButton'>Optional button -&gt;</a>
+            <a href="" className='titleButton'>Optional button<img src={rightArrow} alt="right pointing arrow" /></a>
         </div>
     )
 }
