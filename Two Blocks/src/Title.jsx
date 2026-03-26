3765d3fbac62c4f344da244b8c0dacde65d02a70
SuperTNT17 <r.lemmers@outlook.com>
2026-03-26 16:17:44 +0100

    mobile layout gemaakt

--- Two Blocks/src/Title.jsx
+++ Two Blocks/src/Title.jsx
@@ -6,7 +6,7 @@ export function Title() {
         <div className='titleContainer'>
             <div className='labelTitleContainer'>
                 <p className='titleLabel'>Label</p>
-                <h1 className='title'>Mandatory title about the<br></br><span>call-to-action</span> here</h1>
+                <h1 className='title'>Mandatory title about the <span>call-to-action</span> here</h1>
             </div>
             <p className='titleBody'>Optional body copy goes here.</p>
             <a href="" className='titleButton'>Optional button<img src={rightArrow} alt="right pointing arrow" /></a>
